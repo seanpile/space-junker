3e4fa202b3bf80f0e4b4e9ff0ae29821c053221f
seanpile <sean.dawson@gmail.com>
2017-03-10 11:31:08 -0500

    Moved vendor libraries into the vendor bundle

--- src/vendor.js
+++ src/vendor.js
@@ -1,5 +1,9 @@
 // Place third-party libraries here so that they can be split into
 // a separate bundle.
-require('three');
 require('moment');
 require('stats.js');
+require('splash-screen');
+
+const THREE = require('three');
+require('three-orbit-controls')(THREE);
+require('three-collada-loader');
