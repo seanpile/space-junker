25b010e02cda6c5e6f40d8b20d55c1ee3309ce75
seanpile <sean.dawson@gmail.com>
2017-03-31 13:31:11 -0400

    Remove dependency on airbnb eslint config; manually setup rules

---
+++ src/app/model/orbits/Orbit.js
@@ -0,0 +1,20 @@
+
+export default class Orbit {
+
+  static supports(e) {
+    throw new Error('unimplemented method');
+  }
+
+  setFromKeplerElements(keplerElements, t) {
+    throw new Error('unimplemented method');
+  }
+
+  setFromCartesian(position, velocity) {
+    throw new Error('unimplemented method');
+  }
+
+  advance(dt) {
+    throw new Error('unimplemented method');
+  }
+
+}
